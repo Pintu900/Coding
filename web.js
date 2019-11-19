ec9ad143e5392185d4b51079a92b4245b03e49ea
unknown <shahpintu900@gmail.com>
2019-11-19 19:32:16 +0530

    hello123

---
+++ web.js
@@ -0,0 +1,9 @@
+
+function mousehover(x){
+    document.getElementById(x).style.backgroundImage = "linear-gradient(rgba(0,0,0,0.7),rgba(0,0,0,0.7))";
+    document.getElementById(x).style.borderRadius="24px";
+    document.getElementById(x).style.display = "block";
+}
+function hide(x){
+    document.getElementById(x).style.display = "none";
+}
